97c53d3b3f38e1f7f93d28ab4d98e44b6fe358b8
print('helloworld') <maxwellmacedo2015@gmail.com>
2020-09-23 23:04:42 -0300

    Feature: News enviroment variables 'SENDER_MAIL' and 'SENDER_PASS'

--- config/mail.js
+++ config/mail.js
@@ -1,5 +1,5 @@
 export default {
   MAIL_PORT: 465,
-  MAIL_USER: 'mailsender.next@gmail.com',
-  MAIL_PASS: 'mailsender2020',
+  MAIL_USER: process.env.SENDER_EMAIL,
+  MAIL_PASS: process.env.SENDER_PASS,
 };
